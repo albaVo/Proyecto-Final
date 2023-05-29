f5aca5088c25da36198fd732c5aac79550dd5f2c
albaVo <alba12vo@gmail.com>
2023-05-29 17:14:53 +0200

    panel producto

--- app-tienda/src/components/producto/Panel/Panel.tsx
+++ app-tienda/src/components/producto/Panel/Panel.tsx
@@ -1,10 +1,14 @@
 //styles
-import { Container } from "@mui/material"
+import { Button, Container } from "@mui/material"
 import styles from "./Panel.module.scss"
+import { Check, LocalOffer } from "@mui/icons-material"
+import { fn } from "@/utils"
 
 export const Panel = (props: any) => {
     
     const { producto, productoId } = props
+
+    const buyPrice = fn.calcDiscount(producto.precio, producto.descuento)
     
     return (
         <Container className={styles.panel}>
@@ -21,7 +25,32 @@ export const Panel = (props: any) => {
                             <img src={producto.categoria.icono}/>
                             {producto.categoria.titulo}
                         </span>
+                        <span>
+                            <Check/>
+                            En stock
+                        </span>
                     </div>
+
+                    <div className={styles.price}>
+                        {producto.descuento > 0 && (
+                            <>
+                                <span className={styles.originalPrice}>
+                                    <LocalOffer/>
+                                    {producto.precio}€
+                                </span>
+
+                                <span className={styles.discount}>
+                                    -{producto.descuento}%
+                                </span>
+                            </>
+                        )}
+
+                        <span className={styles.price}>{buyPrice}€</span>
+                    </div>
+
+                    <Button sx={{textDecoration: 'none'}} fullWidth>
+                        Comprar ahora
+                    </Button>
                 </div>
             </div>
         </Container>
