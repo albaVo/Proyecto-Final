7f6a2f20a8001b1f1283beaef485be038835c95c
albaVo <alba12vo@gmail.com>
2023-06-01 15:13:20 +0200

    stepper

--- app-tienda/src/components/layout/HeaderCart/HeaderCart.tsx
+++ app-tienda/src/components/layout/HeaderCart/HeaderCart.tsx
@@ -91,11 +91,6 @@ export const HeaderCart = () => {
                                 Back
                                 </Button>
                                 <Box sx={{ flex: '1 1 auto' }} />
-                                {isStepOptional(activeStep) && (
-                                <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
-                                    Skip
-                                </Button>
-                                )}
                                 <Button onClick={handleNext}>
                                 {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
                                 </Button>
