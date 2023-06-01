7b7a9bacd6b5a711048b82a0f5799fb760ac1019
albaVo <alba12vo@gmail.com>
2023-06-01 10:16:12 +0200

    añadiendo más de un producto

--- app-tienda/src/api/Cart.ts
+++ app-tienda/src/api/Cart.ts
@@ -1,7 +1,17 @@
 
 export class Cart {
     add(productoId: any) {
-        localStorage.setItem('cart', JSON.stringify([productoId]))
+        const productos = this.getAll()
+        const objIndex = productos.findIndex((producto) => producto.id === productoId)
+
+        if(objIndex < 0) {
+            productos.push({ id: productoId, quantity: 1})
+        } else {
+            const producto = productos[objIndex]
+            producto[objIndex].quantity = producto.quantity + 1
+        }
+
+        localStorage.setItem('cart', JSON.stringify(productos))
     }
 
     getAll() {
