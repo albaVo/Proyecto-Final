0b6920f737e755122fbf27356305e36dafa7daa8
albaVo <alba12vo@gmail.com>
2023-06-01 09:48:21 +0200

    obtener productos carrito

--- app-tienda/src/api/Cart.ts
+++ app-tienda/src/api/Cart.ts
@@ -3,4 +3,16 @@ export class Cart {
     add(productoId: any) {
         localStorage.setItem('cart', JSON.stringify([productoId]))
     }
+
+    getAll() {
+        const response = localStorage.getItem('cart')
+
+        if (!response) {
+            return []
+        } else {
+            return JSON.parse(response)
+        }
+
+        return response
+    }
 }
