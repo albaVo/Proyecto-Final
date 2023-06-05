74247260a18a51726d3d28ff55fa8b193ced34a0
albaVo <alba12vo@gmail.com>
2023-06-05 14:46:49 +0200

    basket

--- app-tienda/src/components/cart/StepOne/Basket/Basket.tsx
+++ app-tienda/src/components/cart/StepOne/Basket/Basket.tsx
@@ -9,44 +9,51 @@ import { fn } from "@/utils"
 import { useState } from "react"
 //hooks
 import { useCart } from "@/hooks/useCart"
+//lodash
+import { map } from "lodash"
+//semantic-ui
+import { Dropdown } from "semantic-ui-react"
 
 
 export const Basket = (props: any) => {
     
     const { productos } = props
     const { changeQuantityItem, deleteItem } = useCart()
+
+    const options = Array.from({ length: 6 }, (_, index) => {
+        const number = index + 1 
+        return { key: number, text: String(number), value: number}
+    })
     
     return (
         <div className={styles.basket}>
             <h2>Cesta</h2>
 
             <div className={styles.block}>
-                {productos?.map((producto) => (
+                {map(productos, (producto) => (
                     <div key={producto.id} className={styles.product}>
                         <img src={producto.imagen}/>
                         <div>
                             <div className={styles.info}>
                                 <div>
                                     <p>{producto.titulo}</p>
-                                    <p>{producto.categoria?.titulo}</p>
+                                    <p>{producto.categoria.titulo}</p>
                                 </div>
                                 <Delete onClick={() => deleteItem(producto.id)}/>
                             </div>
 
                             <div className={styles.quantity}>
-                                <Select 
+                                <Dropdown 
+                                    className="number"
+                                    options={options}
+                                    selection
                                     value={producto.quantity}
+                                    compact
                                     onChange={(_, data) => changeQuantityItem(producto.id, data.value)}
-                                >
-                                    <MenuItem value={1}>1</MenuItem>
-                                    <MenuItem value={2}>2</MenuItem>
-                                    <MenuItem value={3}>3</MenuItem>
-                                    <MenuItem value={4}>4</MenuItem>
-                                    <MenuItem value={5}>5</MenuItem>
-                                    <span>
-                                        {fn.calcDiscount(producto.precio, producto.descuento)} €
-                                    </span>
-                                </Select>
+                                />
+                                <span>
+                                    {fn.calcDiscount(producto.precio, producto.descuento)} €
+                                </span>
                             </div>
                         </div>
                     </div>
